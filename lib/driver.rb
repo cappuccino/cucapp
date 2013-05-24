65bd8a9f3bc731b1a4e79631715487ef638e6355
Antoine Mercadal <antoine.mercadal@gmail.com>
2013-05-24 16:55:05 -0700

    Update waiting socket

--- lib/driver.rb
+++ lib/driver.rb
@@ -5,29 +5,29 @@ require 'mysql'
 
 module Driver
   class WebDriver
-    
+
     attr_reader :url
     attr_reader :total
     attr_reader :dbResult
-    
+
     def initialize()
     end
-    
+
     def setTotal(total)
       @total = total
     end
-    
+
     def getTotal()
       return @total
     end
-    
+
     def getURL()
       return @url
     end
-    
+
     def setURL(url)
-	@url = "#{url}"
-    end 
+  @url = "#{url}"
+    end
 
     def getHost()
       host = @url.gsub /^http(s?):\/\//, ''
@@ -39,19 +39,19 @@ module Driver
       puts "Starting to load #{vmloop} VMs to enterprise #{enterprise} under subnet #{subnet} from JMeter......\nIt may take a while......"
       dir = "/Users/Shared/Jenkins/Home/SharedWorkspace/jmeter/CNA_JMETER/bin"
       Dir.chdir("#{dir}") do
-	output = `jmeter  -nt ALU/CNA_GUI_VM_LOADING.jmx -Denterprise_name=#{enterprise} -Ddomain_name=#{domain} -Dzone_name=#{zone} -Dsubnet_name=#{subnet} -Dvmloop=#{vmloop}&`
+  output = `jmeter  -nt ALU/CNA_GUI_VM_LOADING.jmx -Denterprise_name=#{enterprise} -Ddomain_name=#{domain} -Dzone_name=#{zone} -Dsubnet_name=#{subnet} -Dvmloop=#{vmloop}&`
       end
     end
 
     def runSQL(database, sql)
       remote_host = getHost()
-      db_user	  = "cnauser"
-      db_pw	  = "cnapass"
-      db_name	  = "cnadb"
+      db_user   = "cnauser"
+      db_pw   = "cnapass"
+      db_name   = "cnadb"
       conn = Mysql.real_connect(remote_host,db_user,db_pw,db_name)
       res = conn.query "#{sql}"
       row = res.fetch_row
-      return row 
+      return row
     end
 
     def saveDBResult(result)
@@ -59,18 +59,18 @@ module Driver
     end
 
     def getDBResult()
-      return @dbResult 
+      return @dbResult
     end
 
     def getTotalNumIdentifier(entity)
       if (entity == "enterprise")
-	return "field_total_NUEnterprisesViewController"  
+  return "field_total_NUEnterprisesViewController"
       elsif (entity == "domain")
-	return "field_total_NUDomainsViewController"
+  return "field_total_NUDomainsViewController"
       elsif (entity == "zone")
-	return "field_total_NUZonesViewController"
+  return "field_total_NUZonesViewController"
       elsif (entity == "subnet")
-	return "field_total_NUSubnetsViewController"
+  return "field_total_NUSubnetsViewController"
       end
     end
 
@@ -82,44 +82,43 @@ module Driver
       s.close
     end
 
-    def waitMsg(msg, sender)
-      puts "wait message from #{sender}.."
+      def waitMsg(msg, sender)
+  puts "wait message from #{sender}.."
+  portnumber = 2000
+  socketServer = TCPServer.open(portnumber)
+  s = socketServer.accept
+  on = 1
+  while (on == 1)
+    Thread.new(s) do |connection|
+    puts "Accepting connection from: #{connection.peeraddr[2]}"
 
-portnumber = 2000
-socketServer = TCPServer.open(portnumber)
-
-while true
-Thread.new(socketServer.accept) do |connection|
-puts "Accepting connection from: #{connection.peeraddr[2]}"
-
-begin
-while connection
-incomingData = connection.gets("\0")
-if incomingData != nil
-incomingData = incomingData.chomp
-end
-
-puts "Incoming: #{incomingData}"
-
-if incomingData == "DISCONNECT\0"
-puts "Received: DISCONNECT, closed connection"
-connection.close
-break
-else
-connection.puts "#{incomingData}"
-connection.flush
-end
-end
-rescue Exception => e
+    begin
+      while connection
+        incomingData = connection.gets("\0")
+        if incomingData != nil
+    incomingData = incomingData.chomp
+        end
+        puts "Incoming: #{incomingData}"
+        if incomingData == "proceed"
+    puts "proceed the test case, closed connection"
+    connection.close
+    on = 0
+    break
+        else
+    connection.puts "#{incomingData}"
+    connection.flush
+        end
+      end
+    rescue Exception => e
 # Displays Error Message
 #       puts "#{ e } (#{ e.class })"
 #           ensure
 #                 connection.close
 #                       puts "ensure: Closing"
-                           end
-                             end
-                             end
-
+          end
+  end
+      end
     end
-  end    
+  end
 end
+tabs
