dce2054d608ac60176981e98fa7a44d52b58c723
Alexandre Wilhelm <contact.dogild@gmail.com>
2014-05-06 18:03:40 -0700

    Clean Cucapp

--- lib/application.rb
+++ lib/application.rb
@@ -1,7 +1,4 @@
 require 'encumber'
-require 'driver'
-require 'NUEncumber'
-require 'logger'
 
 class Cucapp
   class ExpectationFailed < RuntimeError
@@ -9,26 +6,14 @@ class Cucapp
 
   attr_reader :warning_text
   attr_reader :gui
-  attr_reader :driver
-  attr_reader :port
-  attr_accessor :logger
+
   def initialize
-    @driver = Driver::WebDriver.new
-    @port = Driver::WebDriver.getPort
-    @gui = VSDGUI.new(30, port)
+    @gui = Encumber::GUI.new
     @gui.launch
   end
 
-  def reset()
+  def reset
     @gui.command 'restoreDefaults'
-  end
-
-  def set_env(url, log)
-    driver.setURL("#{url}")
-
-    @logger = Logger.new("#{log}")
-    logger.datetime_format = '%m-%d-%y %H:%M:%S'
-
   end
 
   def quit
